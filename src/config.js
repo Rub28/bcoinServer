13bf7fe43bcb388ad7c184d62f78608fccfc47bd
Rub28 <ruhernandezm@gmail.com>
2025-03-06 17:28:41 -0600

    Se agrega archivo ip en vez de localhost

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ module.exports = {
         secret: process.env.JET_SECRET || 'miconstraseña2848330'
     },
     mysql:{
-        host: process.env.MYSQL_HOST || '127.0.0.1',
+        host: process.env.MYSQL_HOST || '82.197.82.77',
         user: process.env.MYSQL_USER || 'u785010228_rub',
         password: process.env.MYSQL_PASSWORD || 'rub11H28',
         database: process.env.MYSQL_DB || 'u785010228_bcoin_bd', 
