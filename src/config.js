61bc8f6568aa1e86ea5b1f6180845ad1481bb9cf
Rub28 <ruhernandezm@gmail.com>
2025-03-06 17:16:15 -0600

    Se agrega archivo ip en vez de localhost

--- src/config.js
+++ src/config.js
@@ -10,8 +10,8 @@ module.exports = {
     mysql:{
         host: process.env.MYSQL_HOST || '127.0.0.1',
         user: process.env.MYSQL_USER || 'root',
-        password: process.env.MYSQL_PASSWORD || '',
-        database: process.env.MYSQL_DB || 'bcoin_bd', 
+        password: process.env.MYSQL_PASSWORD || 'rub11H28',
+        database: process.env.MYSQL_DB || 'u785010228_bcoin_bd', 
         port: process.env.PORT_BD || 3306  
     }
 }
