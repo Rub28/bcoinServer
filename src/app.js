0203108cab393f348e19913cf66456761818b3be
Rub28 <ruhernandezm@gmail.com>
2025-03-09 21:30:29 -0600

    cambio se agrega certificados

--- src/app.js
+++ src/app.js
@@ -47,8 +47,8 @@ const options = {
   https.createServer(options, (req, res) => {
     res.writeHead(200);
     res.end('¡Hola, mundo seguro!');
-  }).listen(443, () => {
-    console.log('Servidor HTTPS escuchando en el puerto 443');
+  }).listen(4002, () => {
+    console.log('Servidor HTTPS escuchando en el puerto 4002');
   });
 
 
