ac6a4c110e44404a4cda25e58065fe7bd7c36a94
Rub28 <ruhernandezm@gmail.com>
2025-03-09 21:26:01 -0600

    cambio se agrega certificados

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const config = require('./config');
 const cors = require('cors');
+const fs = require('fs');
 
 const clientes = require('./modulos/clientes/rutas');
 const usuarios = require('./modulos/Usuarios/rutas');
